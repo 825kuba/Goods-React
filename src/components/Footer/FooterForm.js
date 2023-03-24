a60b295c7001f9c005a96933386fcdbdd0ae13fb
825kuba <825kuba@gmail.com>
2023-03-24 13:23:47 +0100

    Added footer form functionality

--- src/components/Footer/FooterForm.js
+++ src/components/Footer/FooterForm.js
@@ -1,13 +1,46 @@
+import React, { useState, useRef } from 'react';
+
 import classes from './FooterForm.module.scss';
 
 const FooterForm = props => {
+  const [formSubmited, setFormSubmited] = useState(false);
+
+  const inputRef = useRef();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    if (inputRef.current.value === '') return;
+    setFormSubmited(true);
+  };
+
+  // FORM INNER JSX DEPENDING ON STATE
+  let formInnerJSX;
+
+  if (!formSubmited)
+    formInnerJSX = (
+      <React.Fragment>
+        <input type="email" placeholder="Enter email" ref={inputRef} />
+        <button type="submit">Subsribe</button>
+      </React.Fragment>
+    );
+  else
+    formInnerJSX = (
+      <React.Fragment>
+        <p>Thank you for subscribing!</p>
+        <p>✔</p>
+      </React.Fragment>
+    );
+
   return (
     <div className={classes.subscribe}>
       <h3>Keep in touch</h3>
       <p>Sign up to receive news and updates.</p>
-      <form action="">
-        <input type="email" placeholder="Enter email" />
-        <button type="submit">Subsribe</button>
+      <form
+        action=""
+        onSubmit={submitHandler}
+        className={`${formSubmited ? `${classes.success}` : ''} `}
+      >
+        {formInnerJSX}
       </form>
     </div>
   );
